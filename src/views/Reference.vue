ec2c59ab7a9edb6328b51295fd46d12d619c5599
ksmithut <github@ksmithut.com>
2023-09-24 21:00:33 -0600

    makes reference page more static-like

--- src/views/Reference.vue
+++ src/views/Reference.vue
@@ -1,20 +1,53 @@
 <template>
-<v-main>
+  <v-main>
     <div class="main">
-        <v-row class="mt-15">
-            <v-col v-for="category in categories" :key="category.id" sm='6' lg="3">
-                
-                <h2 id="short-shelter">{{ category.label }} References</h2>
-                <ul>
-                <li v-for="(link,desc) in category.links" :key="link">
-                     {{desc}} <br>
-                    <a :href=link>{{ link }}</a>
-                </li>
-                </ul>
-            </v-col>
-        </v-row>
+      <v-row class="mt-15">
+        <v-col sm="6" lg="3">
+          <h2>Shelter/Clothing References</h2>
+          <ul>
+            <li>
+              google
+              <br />
+              <a href="https://google.com" target="_blank" rel="noreferrer noopener">https://google.com</a>
+            </li>
+          </ul>
+        </v-col>
+
+        <v-col sm="6" lg="3">
+          <h2>Heat/Light/Power/Fuel References</h2>
+          <ul>
+            <li>
+              google
+              <br />
+              <a href="https://www.disasterpreparer.com" target="_blank" rel="noreferrer noopener">https://www.disasterpreparer.com</a>
+            </li>
+          </ul>
+        </v-col>
+
+        <v-col sm="6" lg="3">
+          <h2>Transportation/Comm/Evac</h2>
+          <ul>
+            <li>
+              google
+              <br />
+              <a href="https://urbanaquaponics.com" target="_blank" rel="noreferrer noopener">https://urbanaquaponics.com</a>
+            </li>
+            <li>
+              google
+              <br />
+              <a href="https://google.com" target="_blank" rel="noreferrer noopener">https://google.com</a>
+            </li>
+            <li>
+              google
+              <br />
+              <a href="https://beready.utah.gov" target="_blank" rel="noreferrer noopener">https://beready.utah.gov</a>
+            </li>
+          </ul>
+        </v-col>
+        
+      </v-row>
     </div>
-</v-main>
+  </v-main>
 </template>
 
 <script>
@@ -22,22 +55,14 @@ import data from '../assets/data.json'
 
 export default {
   name: 'App',
-  components: {
-
-  },
+  components: {},
   data: () => ({
-    categories: data.categories.filter(category => category.links?.length)
+    categories: data.categories.filter((category) => category.links?.length)
   }),
-  methods: {
-    
-  },
-  watch:{
-    
-  },
-  created(){
-
-    }
-};
+  methods: {},
+  watch: {},
+  created() {}
+}
 </script>
 
 <style scoped>
